b9f10e3986e397992002f4a5f471691cd1ff925f
Torin Felton <torinbrody1@gmail.com>
2020-11-01 20:16:58 +0000

    Add multi-digit number functionality.

---
+++ Sandbox/Algorithms/ExpressionToken.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sandbox.Algorithms
+{
+    class ExpressionToken
+    {
+        public string value = null;
+        public bool isNumber = false;
+
+        public ExpressionToken(string inputValue, bool isNumber)
+        {
+            this.isNumber = isNumber;
+            this.value = inputValue;
+        }
+
+    }
+}
